6f99ed44e1f6a69ce5f12af72d7f9fd76aacf0cb
nukosuke <nukosuke@users.noreply.github.com>
2022-04-15 14:58:46 +0900

    Add editor.tsx

---
+++ next/pages/editor.tsx
@@ -0,0 +1,27 @@
+import { NextPage } from "next";
+import LexicalComposer from '@lexical/react/LexicalComposer'
+import LexicalPlainTextPlugin from '@lexical/react/LexicalPlainTextPlugin'
+import LexicalContentEditable from '@lexical/react/LexicalContentEditable'
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
+import LexicalOnChangePlugin from '@lexical/react/LexicalOnChangePlugin'
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
+
+const Editor: NextPage = () => {
+  const initialConfig = {
+    theme: {},
+    onError: () => { },
+  }
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <LexicalPlainTextPlugin
+        contentEditable={<LexicalContentEditable />}
+        placeholder={<div>placeholder ...</div>}
+      />
+      <LexicalOnChangePlugin onChange={() => { }} />
+      <HistoryPlugin />
+    </LexicalComposer>
+  )
+}
+
+export default Editor
