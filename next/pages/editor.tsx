24e021102d5ba19932ded8477268bd64c407e623
nukosuke <nukosuke@users.noreply.github.com>
2022-08-21 23:23:39 +0900

    Use with chakra UI (#2)
    * Use pnpm
    
    * Bump packages
    
    * Add chakra-ui
    
    * Fix
    
    * Apply chakra-ui
    
    * Remove unused style

--- next/pages/editor.tsx
+++ next/pages/editor.tsx
@@ -1,31 +1,52 @@
 import { NextPage } from "next";
-import LexicalComposer from '@lexical/react/LexicalComposer'
-import LexicalPlainTextPlugin from '@lexical/react/LexicalPlainTextPlugin'
-import LexicalContentEditable from '@lexical/react/LexicalContentEditable'
+import { LexicalComposer } from '@lexical/react/LexicalComposer'
+import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin'
+import { ContentEditable } from '@lexical/react/LexicalContentEditable'
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
-import LexicalOnChangePlugin from '@lexical/react/LexicalOnChangePlugin'
-import LexicalAutoFocusPlugin from '@lexical/react/LexicalAutoFocusPlugin'
-import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
+import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin'
+import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin'
+import { Container, Text } from "@chakra-ui/react";
+import { $getRoot, $getSelection, EditorState } from "lexical";
 
 const Editor: NextPage = () => {
   const initialConfig = {
+    namespace: 'lexical-practice',
     theme: {},
-    onError: (err: Error) => {
-      throw err
-    },
+    onError: console.error
   }
 
   return (
-    <LexicalComposer initialConfig={initialConfig}>
-      <LexicalPlainTextPlugin
-        contentEditable={<LexicalContentEditable />}
-        placeholder={<div>placeholder ...</div>}
-      />
-      <LexicalOnChangePlugin onChange={() => { }} />
-      <HistoryPlugin />
-      <LexicalAutoFocusPlugin />
-    </LexicalComposer>
+    <Container py={4}>
+      <LexicalComposer initialConfig={initialConfig}>
+        <PlainTextPlugin
+          contentEditable={<ContentEditable />}
+          placeholder={<Placeholder />}
+        />
+        <OnChangePlugin onChange={(editorState: EditorState) => {
+          editorState.read(() => {
+            const root = $getRoot()
+            const selection = $getSelection()
+            console.log(root, selection)
+          })
+        }} />
+        <HistoryPlugin />
+        <AutoFocusPlugin />
+      </LexicalComposer>
+    </Container>
   )
 }
+
+const Placeholder: React.FC = () => (
+  <Text
+    color='gray.400'
+    display='inline-block'
+    overflow='hidden'
+    position='relative'
+    top='-1.5em'
+    left='.25em'
+    userSelect='none'
+    pointerEvents='none'
+  >Enter some plain text...</Text>
+)
 
 export default Editor
