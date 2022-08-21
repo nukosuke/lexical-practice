9a4006f76690b0e53f4e4be630ed908461725047
nukosuke <nukosuke@users.noreply.github.com>
2022-08-21 22:56:50 +0900

    Use Lexical on NextJS (#1)
    * Create Next app
    
    * Add lexical @lexical/react
    
    * Add editor.tsx
    
    * Add AutoFocus
    
    * Fix index
    
    * throw err

---
+++ next/pages/editor.tsx
@@ -0,0 +1,31 @@
+import { NextPage } from "next";
+import LexicalComposer from '@lexical/react/LexicalComposer'
+import LexicalPlainTextPlugin from '@lexical/react/LexicalPlainTextPlugin'
+import LexicalContentEditable from '@lexical/react/LexicalContentEditable'
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
+import LexicalOnChangePlugin from '@lexical/react/LexicalOnChangePlugin'
+import LexicalAutoFocusPlugin from '@lexical/react/LexicalAutoFocusPlugin'
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
+
+const Editor: NextPage = () => {
+  const initialConfig = {
+    theme: {},
+    onError: (err: Error) => {
+      throw err
+    },
+  }
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <LexicalPlainTextPlugin
+        contentEditable={<LexicalContentEditable />}
+        placeholder={<div>placeholder ...</div>}
+      />
+      <LexicalOnChangePlugin onChange={() => { }} />
+      <HistoryPlugin />
+      <LexicalAutoFocusPlugin />
+    </LexicalComposer>
+  )
+}
+
+export default Editor
