8f5542d180f046e2f341a870ec3d172ab28eadec
nukosuke <nukosuke@users.noreply.github.com>
2022-08-21 23:21:21 +0900

    Apply chakra-ui

--- next/pages/editor.tsx
+++ next/pages/editor.tsx
@@ -5,27 +5,48 @@ import { ContentEditable } from '@lexical/react/LexicalContentEditable'
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
 import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin'
 import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin'
+import { Container, Text } from "@chakra-ui/react";
+import { $getRoot, $getSelection, EditorState } from "lexical";
 
 const Editor: NextPage = () => {
   const initialConfig = {
     namespace: 'lexical-practice',
     theme: {},
-    onError: (err: Error) => {
-      throw err
-    },
+    onError: console.error
   }
 
   return (
-    <LexicalComposer initialConfig={initialConfig}>
-      <PlainTextPlugin
-        contentEditable={<ContentEditable />}
-        placeholder={<div>placeholder ...</div>}
-      />
-      <OnChangePlugin onChange={() => { }} />
-      <HistoryPlugin />
-      <AutoFocusPlugin />
-    </LexicalComposer>
+    <Container py={4}>
+      <LexicalComposer initialConfig={initialConfig}>
+        <PlainTextPlugin
+          contentEditable={<ContentEditable />}
+          placeholder={<Placeholder />}
+        />
+        <OnChangePlugin onChange={(editorState: EditorState) => {
+          editorState.read(() => {
+            const root = $getRoot()
+            const selection = $getSelection()
+            console.log(root, selection)
+          })
+        }} />
+        <HistoryPlugin />
+        <AutoFocusPlugin />
+      </LexicalComposer>
+    </Container>
   )
 }
+
+const Placeholder: React.FC = () => (
+  <Text
+    color='gray.400'
+    display='inline-block'
+    overflow='hidden'
+    position='relative'
+    top='-1.5em'
+    left='.25em'
+    userSelect='none'
+    pointerEvents='none'
+  >Enter some plain text...</Text>
+)
 
 export default Editor
